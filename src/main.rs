de83dd5c33d180f13d31daf9a87c9e9740eeb3da
Cameron Barnes <cameron_barnes@outlook.com>
2024-01-21 14:03:23 -0800

    Reorganized code and updated it to comply with clippy pedantic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{collections::HashMap, ops::Add, process::Command};
+use std::process::Command;
 
 use once_cell::sync::Lazy;
 use parsing::{get_page_from_path, parse_page};
-use types::{Category, Document, LibraryItem};
 
 mod parsing;
 mod types;
@@ -10,128 +9,23 @@ mod types;
 static IS_WINDOWS: bool = cfg!(windows);
 static HAS_RSYNC: Lazy<bool> = Lazy::new(check_for_rsync);
 
+#[must_use]
 pub fn check_for_rsync() -> bool {
-    Command::new("which")
-        .arg("rsync")
-        .output()
-        .unwrap()
-        .status
-        .success()
-}
+    let result = Command::new("which").arg("rsync").output();
 
-fn to_document(data: (String, u64, String, String)) -> LibraryItem {
-    let (_catname, size, name, url) = data;
-    LibraryItem::Document(Document::new(name, url, size, types::DownloadType::Http))
+    if let Ok(output) = result {
+        output.status.success()
+    } else {
+        false
+    }
 }
 
 fn main() {
-    let mut map: HashMap<String, Vec<(String, u64, String, String)>> = HashMap::new();
-
     let items = parse_page(&get_page_from_path("https://wiki.kiwix.org/wiki/Content"));
 
-    items.into_iter().for_each(|entry| {
-        let name = entry.0.clone();
-        map.entry(name)
-            .and_modify(|e| e.push(entry.clone()))
-            .or_insert(vec![entry]);
-    });
+    let categories = parse_items_into_categories(items);
 
-    let mut root_kiwix = Category::new("Wiki".into(), vec![], false);
-    let mut wikipedia = Category::new("Wikipedia".into(), vec![], false);
-    let mut stack_exchange = Category::new("Stack Exchange".into(), vec![], false);
-    let mut avanti = Category::new("Avanti".into(), vec![], false);
-    let mut root_linux = Category::new("Linux".into(), vec![], false);
-
-    for (key, value) in map.into_iter() {
-        match key.as_str() {
-            "wikipedia" | "wiktionary" | "wikivoyage" | "wikiversity" | "wikibooks"
-            | "wikisource" | "wikiquote" | "wikinews" | "wikispecies" => {
-                let cat = Category::new(key, value.into_iter().map(to_document).collect(), true);
-                wikipedia.add(LibraryItem::Category(cat));
-            }
-            "ted" | "keylearning" | "scienceinthebath" | "aimhi" => {
-                let cat = Category::new(key, value.into_iter().map(to_document).collect(), false);
-                root_kiwix.add(LibraryItem::Category(cat));
-            }
-            "installgentoo" | "gentoo" => {
-                let cat = Category::new(
-                    key.add(" (wiki)"),
-                    value.into_iter().map(to_document).collect(),
-                    true,
-                );
-                let gentoo =
-                    Category::new("Gentoo".into(), vec![LibraryItem::Category(cat)], false);
-                root_linux.add(LibraryItem::Category(gentoo));
-            }
-            "archlinux" => {
-                let cat = Category::new(
-                    key.add(" (wiki)"),
-                    value.into_iter().map(to_document).collect(),
-                    true,
-                );
-                let arch = Category::new("Arch".into(), vec![LibraryItem::Category(cat)], false);
-                root_linux.add(LibraryItem::Category(arch));
-            },
-            "alpinelinux" => {
-                let cat = Category::new(
-                    key.add(" (wiki)"),
-                    value.into_iter().map(to_document).collect(),
-                    true,
-                );
-                let arch = Category::new("Alpine".into(), vec![LibraryItem::Category(cat)], false);
-                root_linux.add(LibraryItem::Category(arch));
-            }
-            _ => {
-                if value.len() == 1 {
-                    let (cat_name, size, _name, url) = &value[0];
-                    let doc = Document::new(key, url.to_owned(), *size, types::DownloadType::Http);
-                    let doc = LibraryItem::Document(doc);
-                    if cat_name.ends_with("stackexchange.com") {
-                        stack_exchange.add(doc);
-                    } else if cat_name.starts_with("avanti") {
-                        avanti.add(doc);
-                    } else if cat_name.eq_ignore_ascii_case("teded")
-                        || cat_name.eq_ignore_ascii_case("tedmed")
-                    {
-                        let cat = Category::new("ted".into(), vec![doc], false);
-                        root_kiwix.add(LibraryItem::Category(cat));
-                    } else {
-                        root_kiwix.add(doc);
-                    }
-                } else {
-                    let cat = Category::new(
-                        key.clone(),
-                        value.into_iter().map(to_document).collect(),
-                        true,
-                    );
-                    let cat = LibraryItem::Category(cat);
-                    if key.ends_with("stackexchange.com") {
-                        stack_exchange.add(cat);
-                    } else if key.starts_with("avanti") {
-                        avanti.add(cat);
-                    } else if key.eq_ignore_ascii_case("teded")
-                        || key.eq_ignore_ascii_case("tedmed")
-                    {
-                        let cat = Category::new("ted".into(), vec![cat], false);
-                        root_kiwix.add(LibraryItem::Category(cat));
-                    } else {
-                        root_kiwix.add(cat);
-                    }
-                }
-            }
-        }
+    for cat in categories {
+        println!("{}", serde_json::to_string(&cat).unwrap());
     }
-
-    root_kiwix.add(LibraryItem::Category(wikipedia));
-    root_kiwix.add(LibraryItem::Category(stack_exchange));
-    root_kiwix.add(LibraryItem::Category(avanti));
-
-    let root_kiwix = LibraryItem::Category(root_kiwix);
-    let root_linux = LibraryItem::Category(root_linux);
-
-    println!(
-        "{}\n{}",
-        serde_json::to_string(&root_kiwix).unwrap(),
-        serde_json::to_string(&root_linux).unwrap()
-    );
 }
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,6 +1,10 @@
+use std::{collections::HashMap, ops::Add};
+
 use once_cell::sync::Lazy;
 use regex::Regex;
 use reqwest::blocking::Client;
+
+use crate::types::{Category, Document, DownloadType, LibraryItem};
 
 pub fn parse_page(input: &str) -> Vec<(String, u64, String, String)> {
     static RE: Lazy<Regex> = Lazy::new(|| {
@@ -17,9 +21,9 @@ pub fn parse_page(input: &str) -> Vec<(String, u64, String, String)> {
             if unit.eq_ignore_ascii_case("kb") {
                 size *= 1024.0;
             } else if unit.eq_ignore_ascii_case("mb") {
-                size *= 1048576.0;
+                size *= 1_048_576.0;
             } else if unit.eq_ignore_ascii_case("gb") {
-                size *= 1073741824.0;
+                size *= 1_073_741_824.0;
             }
             let size = size as u64;
             (
@@ -30,6 +34,117 @@ pub fn parse_page(input: &str) -> Vec<(String, u64, String, String)> {
             )
         })
         .collect()
+}
+
+fn to_document(data: (String, u64, String, String)) -> LibraryItem {
+    let (_catname, size, name, url) = data;
+    LibraryItem::Document(Document::new(name, url, size, DownloadType::Http))
+}
+
+pub fn parse_items_into_categories(items: Vec<(String, u64, String, String)>) -> Vec<LibraryItem> {
+    let mut map: HashMap<String, Vec<(String, u64, String, String)>> = HashMap::new();
+
+    for entry in items {
+        let name = entry.0.clone();
+        map.entry(name)
+            .and_modify(|e| e.push(entry.clone()))
+            .or_insert_with(|| vec![entry]);
+    }
+
+    let mut root_kiwix = Category::new("Wiki".into(), vec![], false);
+    let mut wikipedia = Category::new("Wikipedia".into(), vec![], false);
+    let mut stack_exchange = Category::new("Stack Exchange".into(), vec![], false);
+    let mut avanti = Category::new("Avanti".into(), vec![], false);
+    let mut root_linux = Category::new("Linux".into(), vec![], false);
+
+    for (key, value) in map {
+        match key.as_str() {
+            "wikipedia" | "wiktionary" | "wikivoyage" | "wikiversity" | "wikibooks"
+            | "wikisource" | "wikiquote" | "wikinews" | "wikispecies" => {
+                let cat = Category::new(key, value.into_iter().map(to_document).collect(), true);
+                wikipedia.add(LibraryItem::Category(cat));
+            }
+            "ted" | "keylearning" | "scienceinthebath" | "aimhi" => {
+                let cat = Category::new(key, value.into_iter().map(to_document).collect(), false);
+                root_kiwix.add(LibraryItem::Category(cat));
+            }
+            "installgentoo" | "gentoo" => {
+                let cat = Category::new(
+                    key.add(" (wiki)"),
+                    value.into_iter().map(to_document).collect(),
+                    true,
+                );
+                let gentoo =
+                    Category::new("Gentoo".into(), vec![LibraryItem::Category(cat)], false);
+                root_linux.add(LibraryItem::Category(gentoo));
+            }
+            "archlinux" => {
+                let cat = Category::new(
+                    key.add(" (wiki)"),
+                    value.into_iter().map(to_document).collect(),
+                    true,
+                );
+                let arch = Category::new("Arch".into(), vec![LibraryItem::Category(cat)], false);
+                root_linux.add(LibraryItem::Category(arch));
+            }
+            "alpinelinux" => {
+                let cat = Category::new(
+                    key.add(" (wiki)"),
+                    value.into_iter().map(to_document).collect(),
+                    true,
+                );
+                let arch = Category::new("Alpine".into(), vec![LibraryItem::Category(cat)], false);
+                root_linux.add(LibraryItem::Category(arch));
+            }
+            _ => {
+                if value.len() == 1 {
+                    let (cat_name, size, _name, url) = &value[0];
+                    let doc = Document::new(key, url.to_owned(), *size, DownloadType::Http);
+                    let doc = LibraryItem::Document(doc);
+                    if cat_name.ends_with("stackexchange.com") {
+                        stack_exchange.add(doc);
+                    } else if cat_name.starts_with("avanti") {
+                        avanti.add(doc);
+                    } else if cat_name.eq_ignore_ascii_case("teded")
+                        || cat_name.eq_ignore_ascii_case("tedmed")
+                    {
+                        let cat = Category::new("ted".into(), vec![doc], false);
+                        root_kiwix.add(LibraryItem::Category(cat));
+                    } else {
+                        root_kiwix.add(doc);
+                    }
+                } else {
+                    let cat = Category::new(
+                        key.clone(),
+                        value.into_iter().map(to_document).collect(),
+                        true,
+                    );
+                    let cat = LibraryItem::Category(cat);
+                    if key.ends_with("stackexchange.com") {
+                        stack_exchange.add(cat);
+                    } else if key.starts_with("avanti") {
+                        avanti.add(cat);
+                    } else if key.eq_ignore_ascii_case("teded")
+                        || key.eq_ignore_ascii_case("tedmed")
+                    {
+                        let cat = Category::new("ted".into(), vec![cat], false);
+                        root_kiwix.add(LibraryItem::Category(cat));
+                    } else {
+                        root_kiwix.add(cat);
+                    }
+                }
+            }
+        }
+    }
+
+    root_kiwix.add(LibraryItem::Category(wikipedia));
+    root_kiwix.add(LibraryItem::Category(stack_exchange));
+    root_kiwix.add(LibraryItem::Category(avanti));
+
+    let root_kiwix = LibraryItem::Category(root_kiwix);
+    let root_linux = LibraryItem::Category(root_linux);
+
+    vec![root_kiwix, root_linux]
 }
 
 pub fn get_page_from_path(path: &str) -> String {
